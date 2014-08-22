974e00512e11240a0d3556e8db44586ac9ad0ad5
hujunyi <hujunyi@gmail.com>
2014-08-22 14:08:33 -0400

    Add unicorn script file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid APP_PATH+"/tmp/pids/unicorn.pid"
 stderr_path APP_PATH+"/log/unicorn.log"
 stdout_path APP_PATH+"/log/unicorn.log"
 
-listen "/tmp/unicorn.todo.sock"
+listen "/tmp/unicorn.blog.sock"
 worker_processes 2
 timeout 30
